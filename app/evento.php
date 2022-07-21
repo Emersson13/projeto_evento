fd35952161670bb89d7ccc42caf1aeb6b2ae0202
Bruno Costa <analistabrunocosta@gmail.com>
2022-07-21 20:10:18 -0300

    finalizado função de validação de data

--- app/evento.php
+++ app/evento.php
@@ -1,4 +1,5 @@
 <?php
+date_default_timezone_set("America/Sao_Paulo");// Alterando o fuso horário do servidor
 
 $nomeEvento = $_POST["nomeEvento"];
 $dataEvento = $_POST["dataEvento"];
@@ -14,6 +15,16 @@ var_dump($_POST);
 function validaData($data){
 
     $dataEvento = new DateTime($data);// Esta classe precisa de uma data no padrão americano para funcionar
+    $dataAtual = new DateTime("now");// estamos pegando a data atual
     echo $dataEvento->format("d/m/Y");// mostrando a data no padrão brasileiro
+    echo "<br> A data de hoje é: ";
+    print_r($dataAtual);
+
+    if($dataEvento > $dataAtual){
+        echo "<p>Data do evento cadastrado com sucesso!</p>";
+    }
+    else{
+        echo "<p>A data do evento não pode ser igual ou anterior à data atual</p>";
+    }
 }
 validaData($dataEvento);
